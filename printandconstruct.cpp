090ead63cbc1d7002dd3ff02c79db5a07ec36cd2
Aarushi2021 <aarushi0015.be21@chitkara.edu.in>
2022-05-26 23:48:50 +0530

    print and contruct

---
+++ printandconstruct.cpp
@@ -0,0 +1,10 @@
+ComplexNumbers(int real,int imaginary)
+  {
+    this->real=real;
+    this->imaginary=imaginary;
+  }
+  void print()
+  {
+    cout<<real<<"+"<<"i"<<imaginary<<real<<endl;
+    
+  }
